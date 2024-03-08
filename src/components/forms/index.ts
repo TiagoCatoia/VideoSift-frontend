7725a858e3e98fa0ab59cc4b9fe522ceefded7b8
Tiago Catoia de Souza <133229612+TiagoCatoia@users.noreply.github.com>
2024-03-07 22:52:52 -0300

    Delete src directory

--- src/components/forms/index.ts
+++
@@ -1,7 +0,0 @@
-import FormRoot from "./FormRoot";
-import FormContent from "./FormContent";
-
-export const Forms = {
-  FormRoot,
-  FormContent,
-};
--- src/components/ui/HorizontalLine.tsx
+++
@@ -1,7 +0,0 @@
-import "./HorizontalLine.css";
-
-const HorizontalLine = () => {
-  return <hr className="horizontal-line" />;
-};
-
-export default HorizontalLine;
